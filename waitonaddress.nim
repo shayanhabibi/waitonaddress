1868cb9ca89c766d62bcffe1484fced6be75046c
shayanhabibi <shayanftw@gmail.com>
2021-12-22 10:59:38 +0800

    define error_timeout for usr convenience

--- waitonaddress.nim
+++ waitonaddress.nim
@@ -1,5 +1,6 @@
 const
   INFINITE* = -1
+  ERROR_TIMEOUT* = 0x5B4    ## Operation returned because timeout period expired
 
 proc waitOnAddress*[T](address: ptr T; compare: ptr T; size: int32;
                       dwMilliseconds: int32): bool {.stdcall, dynlib: "API-MS-Win-Core-Synch-l1-2-0", importc: "WaitOnAddress".}
